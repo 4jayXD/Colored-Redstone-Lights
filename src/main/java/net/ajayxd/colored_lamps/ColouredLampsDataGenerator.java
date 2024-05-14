d2fc90cd46a670b8381155adacb62bf9db5d6504
Ajay <87275549+AjayWasTaken@users.noreply.github.com>
2024-05-14 03:32:13 +0100

    Add files via upload

---
+++ src/main/java/net/ajayxd/colored_lamps/ColouredLampsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ajayxd.colored_lamps;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ColouredLampsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
